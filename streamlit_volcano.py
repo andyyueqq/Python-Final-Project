611ad1f9ac9361c89d80666c04b743757e31482d
andyyueqq <xyyue17@gmail.com>
2025-09-19 21:51:54 -0400

    Update streamlit_volcano.py

--- streamlit_volcano.py
+++ streamlit_volcano.py
@@ -27,10 +27,8 @@ st.markdown(
         background-repeat: no-repeat; 
         background-attachment: fixed; 
     }
-
-    /* sidebar  */
     .css-1d391kg {  
-        background-color: rgba(255, 255, 255, 0.5);
+        background-color: rgba(255, 255, 255, 0.2);
     }
     </style>
     """,
